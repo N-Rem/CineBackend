89681a10c64243ae5d1dd26ee8a84e08b899a8a8
N-Rem <nicolasromero.barrios@gmail.com>
2024-08-07 23:21:39 -0300

    Add project files.

---
+++ Cine/Controllers/Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControllerController : ControllerBase
+    {
+    }
+}
