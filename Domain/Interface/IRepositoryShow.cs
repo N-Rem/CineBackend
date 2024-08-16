340eb80157ca9ba911450132cefbae3f50f71c61
JuaniTorti <nachi.tortjuani@gmail.com>
2024-08-16 19:18:49 -0300

    Reopsitory + Service + Controller of Show

---
+++ Domain/Interface/IRepositoryShow.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public interface IRepositoryShow
+    {
+        bool DeleteShow(int idShow);
+        List<Show>? GetShows();
+        List<Show>? GetShowsByMovieId(int movieId);
+        bool ModifyShow(int idShow, string startTime, string date, string price);
+
+        void AddShow(string startTime, string date, string price);
+    }
+}
---
+++ Infrastructure/Data/RepositoryShow.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Entities;
+using Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Data
+{
+    public class RepositoryShow : IRepositoryShow
+    {
+        private readonly ApplicationContext _context;
+        public RepositoryShow(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<Show>? GetShows()
+        {
+            return _context.Shows.ToList();
+        }
+
+        public List<Show>? GetShowsByMovieId(int movieId)
+        {
+            return _context.Shows.Where(s => s.MovieId == movieId).ToList();
+        }
+
+        public bool ModifyShow(int idShow, string startTime, string date, string price)
+        {
+            var showToModify = _context.Shows.FirstOrDefault(s => s.Id == idShow);
+
+            if (showToModify is not null)
+            {
+                showToModify.StartTime = startTime;
+                showToModify.Date = date;
+                showToModify.Price = price;
+
+                _context.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DeleteShow(int idShow)
+        {
+            var showToDelete = _context.Shows.FirstOrDefault(s => s.Id == idShow);
+
+            if (showToDelete is not null)
+            {
+                _context.Shows.Remove(showToDelete);
+                _context.SaveChanges();
+
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public void AddShow(string startTime, string date, string price)
+        {
+            _context.Shows.Add(new Show { StartTime = startTime, Date = date, Price = price });
+            _context.SaveChanges();
+        }
+    }
+}
