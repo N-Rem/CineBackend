47c71f3493dc8f857f79ed2c99eae4faeddc438f
Pilar Lopez <pilarlopez1705@gmail.com>
2024-08-16 20:52:14 -0300

    Director Dependency Injection

--- Cine/Program.cs
+++ Cine/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Domain.Interface;
 using Application.Services;
 using Application.Interfaces;
+using static Domain.Interface.IRepositoryDirector;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,11 +33,12 @@ builder.Services.AddDbContext<ApplicationContext>(dbContextOptions => dbContextO
 //Injections Respository
 builder.Services.AddScoped<IRepositoryMovie, RepositoryMovie>();
 builder.Services.AddScoped<IShowService, ShowService>();
-
+builder.Services.AddScoped<IDirectorServices, DirectorService>();
 
 //Injection Service
 builder.Services.AddScoped<IMovieServices, MovieServices>();
 builder.Services.AddScoped<IRepositoryShow, RepositoryShow>();
+builder.Services.AddScoped<IDirectorRepository, DirectorRepository>();
 
 
 var app = builder.Build();
